64e101b56e5056f34e0cdf5e6c0605f556620f2b
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-02-15 11:54:42 -0300

    Adding card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,28 @@
     
 </ul>
 
+<section class="container">
+    <div class="row">
+        <div class="col s12 m6">
+            <div class="card">
+                <div class="card-image">
+                    <img 
+                        src="{{ asset('assets/paradise.jpeg') }}" 
+                        alt="Card Image"
+                    />
+                    <span class="card-title">Card Title</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red" href=""><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                    <p>
+                        I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
+</section>
 
 @endsection
 
