70ae5d2bab7badbd2f8b0932b97a804a49a2672f
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-02-15 10:51:12 -0300

    laravel skeleton and inicialization materialize

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Link CDN -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+</head>
+<body>
+    
+    @yield('content')
+
+    <!-- Scrtipt CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    @yield('scripts')
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+<nav>
+    <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="">Sass</a></li>
+            <li><a href="">Components</a></li>
+            <li><a href="">Javascript</a></li>
+            <li><a href="">Mobile</a></li>
+        </ul>
+    </div>
+</nav>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><a href="">Sass</a></li>
+    <li><a href="">Sass</a></li>
+    <li><a href="">Sass</a></li>
+    <li><a href="">Sass</a></li>
+    
+</ul>
+
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.sidenav').sidenav();
+    });
+</script>
+@endsection
